164e5949e80843e0bc82dab6a18254e62a205d6e
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-10 21:37:17 -0700

    Add Annimation

--- src/parts/Job.js
+++ src/parts/Job.js
@@ -2,18 +2,26 @@ import React from "react";
 
 import CardSkill from "./CardSkill";
 
-import WEB from '../assets/images/web.png'
-import MOBILE from '../assets/images/mobile.png'
+import WEB from "../assets/images/web.png";
+import MOBILE from "../assets/images/mobile.png";
+
+import Fade from "react-reveal/Fade";
 
 const Degree = () => {
   return (
     <section className="container job">
-      <h4 className="job-title mb-5 mb-md-0">
-        Yang saya<span style={{ color: "#152C5B" }}> Kerjakan</span>
-      </h4>
+      <Fade bottom>
+        <h4 className="job-title mb-5 mb-md-0">
+          Yang saya<span style={{ color: "#152C5B" }}> Kerjakan</span>
+        </h4>
+      </Fade>
       <div className="row d-flex justify-content-center align-items-center">
-      <CardSkill poto={WEB} name="Web Development"></CardSkill>
-      <CardSkill poto={MOBILE} name="Mobile Development"></CardSkill>
+        <Fade bottom delay={300}>
+          <CardSkill poto={WEB} name="Web Development"></CardSkill>
+        </Fade>
+        <Fade bottom delay={600}>
+          <CardSkill poto={MOBILE} name="Mobile Development"></CardSkill>
+        </Fade>
       </div>
     </section>
   );
