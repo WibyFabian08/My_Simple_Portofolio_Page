d895b754915544b8e0098df24c971448a6c8ed55
WibyFabian08 <wibyfabian08@gmail.com>
2021-07-12 16:45:51 -0700

    Add Change

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import Job from '../parts/Job';
-import Footer from '../parts/Footer';
-import Header from '../parts/Header';
-import Hero from '../parts/Hero'
-import Skill from '../parts/Skill';
+import React from "react";
+import Job from "../parts/Job";
+import Footer from "../parts/Footer";
+import Header from "../parts/Header";
+import Hero from "../parts/Hero";
+import Skill from "../parts/Skill";
 
 const MainPage = () => {
-    return (
-        <div className="main-page">
-            <div className="atas">
-                <Header></Header>
-                <Hero></Hero>
-            </div>
-            <Skill></Skill>
-            <Job></Job>
-            <Footer></Footer>
-        </div>
-    )
-}
+  return (
+    <div className="main-page">
+      <div className="atas">
+        <Header></Header>
+        <Hero></Hero>
+      </div>
+      <Skill></Skill>
+      <Job></Job>
+      <Footer></Footer>
+    </div>
+  );
+};
 
 export default MainPage;
